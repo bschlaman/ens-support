00db9d2a2bfcc82b5c91ff1bf84032c25e45584a
Brendan Schlaman <brendan.schlaman@gmail.com>
2020-12-31 11:18:20 -0600

    polishing the rough edges, moving more fields to json config

--- mvs_testing/tek_lifecycle.py
+++ mvs_testing/tek_lifecycle.py
@@ -5,25 +5,26 @@ import json
 import requests
 import os
 import hmac
+from time import time
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
+class acol:
+    HDR = '\033[95m'
+    BLU = '\033[94m'
+    CYN = '\033[96m'
     GRN = '\033[92m'
     YEL = '\033[93m'
     FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
+    END = '\033[0m'
+    BLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-env = "staging"
-health_authority_id = "gov.pwcstage.exposurenotifications"
-key_server = "https://dev.exposurenotification.health/v1/publish"
+env = "prod"
 admin_base_url = ""
 base_url = ""
 admin_key = ""
 device_key = ""
+health_authority_id = ""
+key_server = ""
 
 with open("api.json") as f:
     data = json.load(f)
@@ -31,6 +32,8 @@ with open("api.json") as f:
     base_url = data[env]["base_url"]
     admin_key = data[env]["admin_key"]
     device_key = data[env]["device_key"]
+    health_authority_id = data[env]["health_authority_id"]
+    key_server = data[env]["key_server"]
 
 endpoints = {
     "issue": "/api/issue",
@@ -47,8 +50,8 @@ def issue_code():
     url = admin_base_url + endpoints["issue"]
     headers["x-api-key"] = admin_key
     payload = {
-        "symptomDate": "2020-12-01",
-        "testDate": "2020-12-01",
+        "symptomDate": "2020-12-21",
+        "testDate": "2020-12-21",
         "testType": "confirmed",
         "tzOffset": 0,
         "padding": "asdfasdf"
@@ -97,20 +100,26 @@ def publish_keys(keys, certificate, secret):
     }
     res = requests.post(url, headers=headers, data=json.dumps(payload))
     res_dict = json.loads(res.content.decode("utf-8"))
+    print("=====")
+    print(json.dumps(payload, indent=4))
+    print("=====")
     print(json.dumps(res_dict, indent=4))
 
 def gen_keys(n):
     keys = []
+    rolling_start_interval = int((time() / 86400) * 144)
+    print((acol.YEL + "Current unix time: {}" + acol.END).format(int(time())))
+    print((acol.YEL + "Unix interval time: {}" + acol.END).format(rolling_start_interval))
     for x in range(n):
         tek = {
             "key": base64.b64encode(os.urandom(16)).decode("utf-8"),
-            "rollingStartNumber": 2678110 - x*144,
+            "rollingStartNumber": rolling_start_interval - x*144,
             "rollingPeriod": 144,
             "transmissionRisk": random.randint(1,5)
         }
         keys.append(tek)
     for n,x in enumerate(keys):
-        print(bcolors.OKCYAN + "key {}:".format(n) + bcolors.ENDC + "\n{}".format(json.dumps(keys[n], indent=2)))
+        print(acol.CYN + "key {}:".format(n) + acol.END + "\n{}".format(json.dumps(keys[n], indent=2)))
     return keys
 
 def mutate_keys(keys):
@@ -126,50 +135,42 @@ def gen_hmac(secret, message):
     return str(b, "utf-8")
 
 def main():
-    print(bcolors.HEADER + " === TEK Verification and Publish Tool === " + bcolors.ENDC)
+    print(acol.HDR + " === TEK Verification and Publish Tool === " + acol.END)
 
-    input("\n >>> Press ENTER to generate keys.")
+    input("\n >>> Press "+acol.BLD+"ENTER"+acol.END+" to "+acol.BLD+"generate keys"+acol.END+".")
     print("Generating keys...")
-    keys = gen_keys(40)
+    num_keys = random.randint(2, 5)
+    keys = gen_keys(num_keys)
+    print("Number of keys generated: {}".format(num_keys))
 
-    input("\n >>> Press ENTER to issue a code.")
+    input("\n >>> Press "+acol.BLD+"ENTER"+acol.END+" to "+acol.BLD+"issue a code"+acol.END+".")
     print("Issuing code...")
     code = issue_code()
-    print("code: " + bcolors.YEL + code + bcolors.ENDC)
+    print("code: " + acol.YEL + code + acol.END)
     print("This code will expire in 15 min.")
 
-    input("\n >>> Press ENTER to verify the code")
+    input("\n >>> Press "+acol.BLD+"ENTER"+acol.END+" to "+acol.BLD+"verify the code"+acol.END+".")
     print("Exchanging the code for a long-term token...")
     token = verify_code(code)
-    print("long-term token: " + bcolors.YEL + token[0:19] + "..." + bcolors.ENDC)
+    print("long-term token: " + acol.YEL + token[0:19] + "..." + acol.END)
 
-    input("\n >>> Press ENTER to calculate the HMAC of your keys")
+    input("\n >>> Press "+acol.BLD+"ENTER"+acol.END+" to "+acol.BLD+"calculate the HMAC"+acol.END+" of your keys.")
     secret = bytes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
     secret_str = str(base64.b64encode(secret), "utf-8")
     print("Converting keys into a string...")
     message = mutate_keys(keys)
-    print("key string: " + bcolors.YEL + message + bcolors.ENDC)
+    print("key string: " + acol.YEL + message + acol.END)
     print("Generating HMAC of keys...")
-    print("Using secret: " + bcolors.GRN + secret_str + bcolors.ENDC)
+    print("Using secret: " + acol.GRN + secret_str + acol.END)
     ekeyhmac = gen_hmac(secret, message)
-    print("hmac: " + bcolors.OKCYAN + ekeyhmac + bcolors.ENDC)
+    print("hmac: " + acol.CYN + ekeyhmac + acol.END)
 
-    input("\n >>> Press ENTER to get verification certificate.")
+    input("\n >>> Press "+acol.BLD+"ENTER"+acol.END+" to get your "+acol.BLD+"verification certificate"+acol.END+".")
     certificate = get_certificate(token, ekeyhmac)
-    print("certificate: " + bcolors.YEL + certificate[0:19] + "..." + bcolors.ENDC)
+    print("certificate: " + acol.YEL + certificate[0:19] + "..." + acol.END)
 
-    input("\n >>> Press ENTER to publish keys.")
-    #print(certificate)
+    input("\n >>> Press "+acol.BLD+"ENTER"+acol.END+" to "+acol.BLD+"publish your keys"+acol.END+".")
     publish_keys(keys, certificate, secret_str)
-    # menu = {"1":("issue",issue_code),
-    #         "2":("verify",verify_code),
-    #         "3":("certificate",get_certificate)
-    #    }
-    # for key in sorted(menu.keys()):
-    #      print(key+":" + menu[key][0])
-
-    # ans = input("Make A Choice")
-    # menu.get(ans,[None,exit(1)])[1]()
 
 if __name__ == "__main__":
     main()
